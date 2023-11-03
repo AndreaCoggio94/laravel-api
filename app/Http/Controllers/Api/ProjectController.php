da7799a3c7fe222c2f4bc05e0fbd52efe1f978d7
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-11-03 18:27:19 +0100

    added paginate 12

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -16,7 +16,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        $projects = Project::select('id','type_id','name')->paginate(10);
+        $projects = Project::select('id','type_id','name')->paginate(12);
         return response()->json($projects);
     }
 
